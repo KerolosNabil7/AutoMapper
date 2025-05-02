44b5fb783cc84f2f87bbe85bc91259a378567ebe
Kerolos Nabil <86992138+KerolosNabil7@users.noreply.github.com>
2025-05-02 20:02:26 +0300

    Add MappingProfile class

---
+++ LearnAutoMapper/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LearnAutoMapper.Models;
+
+namespace LearnAutoMapper.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            //Map from Developer Object to DeveloperDTO Object
+            CreateMap<Developer, DeveloperDTO>();
+        }
+    }
+}
