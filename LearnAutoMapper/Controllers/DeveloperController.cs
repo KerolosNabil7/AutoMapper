bea08f6a20881e59f3ebf032e22311489b40e125
Kerolos Nabil <86992138+KerolosNabil7@users.noreply.github.com>
2025-05-02 20:06:50 +0300

    Add DeveloperController

---
+++ LearnAutoMapper/Controllers/DeveloperController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using LearnAutoMapper.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnAutoMapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        public DeveloperController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Post(Developer developer)
+        {
+            var developerDtoMapped = _mapper.Map<DeveloperDTO>(developer);
+            return Ok(developerDtoMapped);
+        }
+    }
+}
