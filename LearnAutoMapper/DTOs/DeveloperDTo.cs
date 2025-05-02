9ca905575a57defccc00438480dc832fc14cbd2c
Kerolos Nabil <86992138+KerolosNabil7@users.noreply.github.com>
2025-05-02 20:00:30 +0300

    Add Models folder Developer model and DTO folder DeveloperDTO

---
+++ LearnAutoMapper/DTOs/DeveloperDTo.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnAutoMapper.Models
+{
+    public class DeveloperDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal Experience { get; set; }
+    }
+}
